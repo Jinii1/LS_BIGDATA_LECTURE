e8cde1aec2a54bdd3fa4149aacdc8d2a90a4d18f
YeonJin Kim <jinaya0904@gmail.com>
2024-07-15 17:17:29 +0900

    test

--- code/textbook-chap4.py
+++ code/textbook-chap4.py
@@ -23,6 +23,7 @@ sum(df['판매량']) / len(df['판매량'])
 
 ! pip install openpyxl
 import pandas as pd
+import numpy as np
 
 df_exam = pd.read_excel('data/excel_exam.xlsx')
 df_exam
@@ -35,7 +36,7 @@ df_exam.shape
 len(df_exam)
 df_exam.size
 
-df_exam = pd.read_excel('data/excel_exam.xlsx', sheet_name ='Sheet2')
+df_exam = pd.read_excel('data/excel_exam.xlsx')
 df_exam
 
 df_exam['total'] = df_exam['math'] + df_exam['math'] + df_exam['science']
@@ -43,15 +44,30 @@ df_exam['mean'] = (df_exam['total']) / len(df_exam['total'])
 
 df_exam[df_exam['math']>50]
 
+# 문제
 df_exam [(df_exam['math']>50) & (df_exam['english']>50)]
 
+# 수학은 평균보다 높지만 영어는 평균보다 낮은 
+mean_m = np.mean(df_exam['math'])
+mean_e = np.mean(df_exam['english'])
 
+df_exam[(df_exam['math'] > mean_m) &
+(df_exam['english'] < mean_e)]
 
+df_nc3 = df_exam [df_exam['nclass'] == 3]
+df_nc3[['math', 'english', 'science']]
+df_nc3[1:]
+df_nc3[0:1]
 
+df_exam[0:10:2]
+df_exam[7:16]
 
+df_exam.sort_values('math', ascending = False)
+df_exam.sort_values(['nclass', 'math'], ascending = [True, False])
 
-
-
+np.where(a>3, 'Up', 'Down')
+df_exam['updown'] = np.where(df_exam['math']>50, 'Up', 'Down')
+df_exam
 
 
 
